5f13b3c97eb340c5cc74597f469d7186305c6f31
herodevcode <134185557+herodevcode@users.noreply.github.com>
2023-12-26 10:12:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ import streamlit as st
 from moviepy.editor import VideoFileClip
 from datetime import time
 
-def download_video(url, output_path, start_time, end_time, video_title):
+def download_video(url, output_path, start_time, end_time, video_title, full_video=False):
     yt = pytube.YouTube(url)
     video = yt.streams.get_highest_resolution()
     temp_output_file = os.path.join(output_path, video.default_filename)
     video.download(output_path=output_path) 
 
-    # Convert time objects to seconds (treating hours as minutes and minutes as seconds)
-    start_seconds = start_time.hour * 60 + start_time.minute
-    end_seconds = end_time.hour * 60 + end_time.minute
+    if not full_video:
+        # Convert time objects to seconds (treating hours as minutes and minutes as seconds)
+        start_seconds = start_time.hour * 60 + start_time.minute
+        end_seconds = end_time.hour * 60 + end_time.minute
 
-    # Cut the video
-    with VideoFileClip(temp_output_file) as video_clip:
-        video_segment = video_clip.subclip(start_seconds, end_seconds)
+        # Cut the video
+        with VideoFileClip(temp_output_file) as video_clip:
+            video_segment = video_clip.subclip(start_seconds, end_seconds)
+            final_output_filename = f"{video_title}.mp4"
+            final_output_file = os.path.join(output_path, final_output_filename)
+            video_segment.write_videofile(final_output_file, codec="libx264", audio_codec="aac")
+    else:
+        # Use the full video as is
         final_output_filename = f"{video_title}.mp4"
         final_output_file = os.path.join(output_path, final_output_filename)
-        video_segment.write_videofile(final_output_file, codec="libx264", audio_codec="aac")
+        os.rename(temp_output_file, final_output_file)
 
-    os.remove(temp_output_file)  # Remove the original, full-length video
+    if not full_video:
+        os.remove(temp_output_file)  # Remove the original, full-length video
     return final_output_file
 
 if __name__ == '__main__':
@@ -33,13 +40,14 @@ if __name__ == '__main__':
     end_time = st.time_input("End Time (Minutes:Seconds)", value=default_end_time)
     
     video_title = st.text_input('Video Title')
-
     url = st.text_input('YouTube URL')
 
-    if url and start_time and end_time and video_title:
+    full_video = st.checkbox('Download entire video without specifying timestamps')
+
+    if url and video_title:
         output_path = 'videos' 
         os.makedirs(output_path, exist_ok=True)
-        filepath = download_video(url, output_path, start_time, end_time, video_title)
+        filepath = download_video(url, output_path, start_time, end_time, video_title, full_video=full_video)
         with open(filepath, 'rb') as f:
             st.download_button(
                 label='Download video',
