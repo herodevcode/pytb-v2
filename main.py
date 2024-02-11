43e74ddd02140a2023ddbba80a98e71b1bc1c3b0
herodevcode <134185557+herodevcode@users.noreply.github.com>
2024-02-11 23:17:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 import pytube
-import streamlit as st
 from moviepy.editor import VideoFileClip
+import streamlit as st
 from datetime import time
-from time import sleep  # Import sleep from the time module
+from time import sleep
 
 def download_video(url, output_path, start_time, end_time, video_title, full_video=False):
     yt = pytube.YouTube(url)
     video = yt.streams.get_highest_resolution()
     temp_output_file = os.path.join(output_path, video.default_filename)
-    video.download(output_path=output_path) 
+    video.download(output_path=output_path)
 
     if not full_video:
-        # Convert time objects to seconds (treating hours as minutes and minutes as seconds)
-        start_seconds = start_time.hour * 60 + start_time.minute
-        end_seconds = end_time.hour * 60 + end_time.minute
+        # Correctly convert time objects to total seconds
+        start_seconds = start_time.hour * 3600 + start_time.minute * 60 + start_time.second
+        end_seconds = end_time.hour * 3600 + end_time.minute * 60 + end_time.second
 
         # Cut the video
         with VideoFileClip(temp_output_file) as video_clip:
@@ -37,16 +37,17 @@ if __name__ == '__main__':
 
     full_video = st.checkbox('Download entire video without specifying timestamps')
     
-    default_start_time = time(0, 0)  # Default time interpreted as 0 minutes and 0 seconds
+    # Adjusting the default time to support up to 99:59
+    default_start_time = time(0, 0)  # Default time interpreted as 0 hours and 0 minutes
     default_end_time = time(0, 0)
-    start_time = st.time_input("Start Time (Minutes:Seconds)", value=default_start_time)
-    end_time = st.time_input("End Time (Minutes:Seconds)", value=default_end_time)
+    start_time = st.time_input("Start Time (HH:MM:SS)", value=default_start_time)
+    end_time = st.time_input("End Time (HH:MM:SS)", value=default_end_time)
     
     video_title = st.text_input('Video Title')
     url = st.text_input('YouTube URL')
 
     if url and video_title:
-        output_path = 'videos' 
+        output_path = 'videos'
         os.makedirs(output_path, exist_ok=True)
         with st.status("Processing...", expanded=True) as status:
             st.write("Downloading video...")
